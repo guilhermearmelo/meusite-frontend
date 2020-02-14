d8c4a3ae4581ab1f046738e872d4acb1a1dccac6
Guilherme Melo <guilhermemelo@frwk.com.br>
2020-02-14 12:44:49 -0300

    #007 - Component Experiencia

---
+++ src/app/experiencia/experiencia.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ExperienciaComponent } from './experiencia.component';
+
+@NgModule({
+  declarations: [ExperienciaComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class ExperienciaModule { }
