1144c9f52d8c407ef0d67426a15aaae3cd5790fb
Guilherme Melo <guilhermemelo@frwk.com.br>
2020-02-14 12:44:25 -0300

    #005 - Component Habilidades

---
+++ src/app/habilidades/habilidades.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HabilidadesComponent } from './habilidades.component';
+
+@NgModule({
+  declarations: [HabilidadesComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class HabilidadesModule { }
