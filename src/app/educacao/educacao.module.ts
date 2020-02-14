23ed1844b074d5e5c28a59135186c07c2c00ea2d
Guilherme Melo <guilhermemelo@frwk.com.br>
2020-02-14 12:45:01 -0300

    #008 - Component Educacao

---
+++ src/app/educacao/educacao.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { EducacaoComponent } from './educacao.component';
+@NgModule({
+  declarations: [EducacaoComponent],
+  imports: [
+    CommonModule
+  ]
+})
+export class EducacaoModule { }
