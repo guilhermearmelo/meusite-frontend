b19bdfd7a2a8218b6829fda46249679bcd4d8306
Guilherme Melo <guilhermemelo@frwk.com.br>
2020-02-14 13:39:04 -0300

    #011 - Fix Layout Navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,19 +4,28 @@ import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { FooterComponent } from './footer/footer.component';
 import { SobreComponent } from './sobre/sobre.component';
 import { EducacaoComponent } from './educacao/educacao.component';
 import { InteressesComponent } from './interesses/interesses.component';
 import { CertificacoesComponent } from './certificacoes/certificacoes.component';
+import { ExperienciaComponent } from './experiencia/experiencia.component';
+import { HabilidadesComponent } from './habilidades/habilidades.component';
+import { PortfolioComponent } from './portfolio/portfolio.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     NavbarComponent,
+    FooterComponent,
     SobreComponent,
     EducacaoComponent,
     InteressesComponent,
-    CertificacoesComponent
+    CertificacoesComponent,
+    ExperienciaComponent,
+    HabilidadesComponent,
+    PortfolioComponent
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   imports: [
