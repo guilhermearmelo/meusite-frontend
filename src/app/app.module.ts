05d10321bed27a43edafe725c28f3891a9087609
Guilherme Melo <guilhermemelo@frwk.com.br>
2020-02-14 12:45:38 -0300

    #010 - Update app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { NgModule, NO_ERRORS_SCHEMA } from '@angular/core';
 import { MDBBootstrapModule } from 'angular-bootstrap-md';
 
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { SobreComponent } from './sobre/sobre.component';
+import { EducacaoComponent } from './educacao/educacao.component';
+import { InteressesComponent } from './interesses/interesses.component';
+import { CertificacoesComponent } from './certificacoes/certificacoes.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    SobreComponent,
+    EducacaoComponent,
+    InteressesComponent,
+    CertificacoesComponent
   ],
   schemas: [ NO_ERRORS_SCHEMA ],
   imports: [
